1066367d2dcdedff0b723e4c2c3be798b89ebb28
Abhishek Agarwal <agarwal.1983@gmail.com>
2020-03-15 17:33:05 -0500

    add Message API and updated console program to make REST call via Configured API Url

---
+++ test-app/test-api/Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace test_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!!!";
+        }
+    }
+}
---
+++ test-app/test-console/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace test_console
+{
+    class Program
+    {
+        static readonly HttpClient _client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            var apiUrl = config["apiUrl"];
+
+            if (string.IsNullOrEmpty(apiUrl))
+            {
+                throw new NullReferenceException($"apiUrl is not configured in appsetting.json");
+            }
+
+            var message = await _client.GetStringAsync(apiUrl);
+
+            Console.WriteLine(message);
+        }
+    }
+}
